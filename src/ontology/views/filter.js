1429b0070551ae9f77270e654f6939495e197ee1
Cristian <cristianvasquez@gmail.com>
2024-04-12 16:12:28 +0200

    add negative filters and wildcards

--- src/ontology/views/filter.js
+++ src/ontology/views/filter.js
@@ -1,11 +1,25 @@
-const startsWith = (value, arr) => arr.some(x => value?.startsWith(x))
+import wcmatch from 'wildcard-match'
+
+const isNegation = (x) => x.startsWith('!') | x.startsWith('-')
+const apply = value => f => value ? f(value) : false
+const matchHasPositives = arr => value => arr.filter(x => !isNegation(x)).
+  map(x => wcmatch(x)).
+  some(apply(value))
+
+const matchHasNegatives = arr => value => arr.filter(isNegation).
+  map(x => wcmatch(x.slice(1))).
+  some(apply(value))
 
 function filterBy ({ nodes, edges }, { filter, includeIncoming = true }) {
+
+  const f = matchHasPositives(filter)
+  const nf = matchHasNegatives(filter)
+
   return {
-    nodes: nodes.filter(x => startsWith(x.name, filter)),
+    nodes: nodes.filter(x => f(x.name)),
     edges: edges.filter(
-      x => startsWith(x.source, filter) || startsWith(x.predicate, filter) ||
-        (includeIncoming && startsWith(x.target, filter))),
+      x => !(nf(x.source) || nf(x.predicate) || nf(x.target)) &&
+        (f(x.source) || f(x.predicate) || includeIncoming && f(x.target))),
   }
 }
 
--- test/filter.test.js
+++ test/filter.test.js
@@ -1,27 +1,41 @@
 import { expect } from 'expect'
 import toMatchSnapshot from 'expect-mocha-snapshot'
 import { describe, it } from 'mocha'
-import {
-  filterBy,
-} from '../src/ontology/views/filter.js'
+import { filterBy } from '../src/ontology/views/filter.js'
 import { getJson } from './support/readEpo.js'
 
 expect.extend({ toMatchSnapshot })
 
 describe('filter', () => {
   const eaJson = getJson()
-  const filter = ['epo-cat:', 'epo-not:']
+
   it(`filterBy incoming=true`, function () {
-    const filtered = filterBy(eaJson, {
-      filter,
-    })
+    const view = {
+      filter: ['epo-cat:*', 'epo-not:*'], includeIncoming: true,
+    }
+
+    const filtered = filterBy(eaJson, view)
     expect(filtered).toMatchSnapshot(this)
   })
+
   it(`filterBy incoming=false`, function () {
 
-    const filtered = filterBy(eaJson, {
-      filter, includeIncoming: false,
-    })
+    const view = {
+      filter: ['epo-cat:*', 'epo-not:*'], includeIncoming: false,
+    }
+
+    const filtered = filterBy(eaJson, view)
+    expect(filtered).toMatchSnapshot(this)
+  })
+
+  it(`filterBy incoming=false small`, function () {
+
+    const view = {
+      filter: ['foaf:Agent', 'epo:Purpose', '!epo:Q*', '-dct*'],
+      includeIncoming: false,
+    }
+
+    const filtered = filterBy(eaJson, view)
     expect(filtered).toMatchSnapshot(this)
   })
 })
