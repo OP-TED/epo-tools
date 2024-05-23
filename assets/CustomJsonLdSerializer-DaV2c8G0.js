2eef9f883c237a4bec519a17e059fb3d3205f4ca
cristianvasquez <cristianvasquez@users.noreply.github.com>
2024-05-23 06:27:10 +0000

    deploy: 7a7dc1ebacd8e30773b2d6962eb6cce731e1015f

---
+++ assets/CustomJsonLdSerializer-DaV2c8G0.js
@@ -0,0 +1,1 @@
+import{S as e}from"./index-IdeggTSM.js";class t extends e{constructor({...r}={}){super({encoding:"string",...r})}}export{t as default};
--- assets/CustomRdfXmlParser-BesPZIGW.js
+++
@@ -1,1 +0,0 @@
-import{r as s}from"./index-B8eoWoro.js";class m extends s.RdfXmlParser{constructor({factory:r,...a}={}){super({...a,dataFactory:r})}}export{m as default};
