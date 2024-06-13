c9bff58a7fc509ec7ee42fad5cb364c7f6e4ffda
Cristian <cristianvasquez@gmail.com>
2024-06-13 15:21:39 +0200

    expected

--- outputs/writeShacl.js
+++ outputs/writeShacl.js
@@ -1,16 +1,58 @@
 import { writeFileSync } from 'fs'
+import { Store } from 'oxigraph'
 import { PRODUCTION } from '../src/config.js'
+import { prettyPrintTurtle } from '../src/io/serialization.js'
 import { toShacl } from '../src/shacl/model2Shacl.js'
 import { getEpoJson } from '../src/epo/readEpo.js'
+import { doConstruct } from '../src/sparql/localStore.js'
 
-const onlyEPO = getEpoJson(PRODUCTION)
+const { databasePath, tag } = PRODUCTION
+const filePrefix = `outputs/shacl/epo_${tag}`
+const onlyEPO = getEpoJson({ databasePath })
 
+/**
+ * Write SHACL
+ */
 const { dataset, turtle, errors } = await toShacl(onlyEPO)
 
-const path = `outputs/shacl/epo.shacl.ttl`
+const path = `${filePrefix}.shacl.ttl`
 writeFileSync(path, turtle)
 console.log('wrote', dataset.size, 'quads at', path)
 
-const errorFile = `outputs/shacl/epo.shacl.errors.json`
+/**
+ * Infer subproperties SHACL
+ */
+const query = `
+PREFIX shacl: <http://www.w3.org/ns/shacl#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+
+CONSTRUCT {
+  ?childShape shacl:property ?parentProperty .
+}
+WHERE {
+   ?child rdfs:subClassOf* ?parent .
+   ?parentShape a shacl:NodeShape ;
+      shacl:targetClass ?parent ;
+      shacl:property ?parentProperty .
+    ?childShape a shacl:NodeShape ;
+    shacl:targetClass ?child .         
+}
+`
+
+const store = new Store()
+for (const quad of dataset) {
+  store.add(quad)
+}
+const inferred = doConstruct({ store, query })
+const inferredFile = `${filePrefix}.shacl.inferred.ttl`
+const prettyTurtle = await
+  prettyPrintTurtle({ dataset: inferred })
+writeFileSync(inferredFile, prettyTurtle)
+console.log('wrote', inferred.size, 'quads at', inferredFile)
+
+/**
+ * Write source errors
+ */
+const errorFile = `${filePrefix}.shacl.errors.json`
 writeFileSync(errorFile, JSON.stringify(errors, null, 2))
 console.log('wrote', errors.edges.length, 'edges with errors at', errorFile)
