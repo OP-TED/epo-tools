07e5c0123150113d24e1a9d306960e37d827aeeb
Cristian <cristianvasquez@gmail.com>
2024-06-14 09:06:55 +0200

    Add rdfs:isDefinedBy

--- outputs/writeOwl.js
+++ outputs/writeOwl.js
@@ -5,6 +5,7 @@ import { prettyPrintTurtle } from '../src/io/serialization.js'
 import { toOwl } from '../src/shacl/model2Owl.js'
 import { getEpoJson } from '../src/epo/readEpo.js'
 import { doConstruct } from '../src/sparql/localStore.js'
+import rdf from 'rdf-ext'
 
 const { databasePath, tag } = PRODUCTION
 const filePrefix = `outputs/owl/epo_${tag}`
@@ -20,9 +21,9 @@ writeFileSync(path, turtle)
 console.log('wrote', dataset.size, 'quads at', path)
 
 /**
- * Infer disjoints OWL
+ * Append OWL disjoints
  */
-const query = `
+const disjoints = `
 PREFIX shacl: <http://www.w3.org/ns/shacl#>
 PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
 PREFIX owl: <http://www.w3.org/2002/07/owl#> 
@@ -37,16 +38,40 @@ WHERE {
 }
 `
 
+/**
+ * Append rdfs:isDefinedBy
+ */
+const isDefinedBy = `
+PREFIX shacl: <http://www.w3.org/ns/shacl#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX owl: <http://www.w3.org/2002/07/owl#> 
+
+CONSTRUCT {
+  ?s rdfs:isDefinedBy <http://data.europa.eu/a4g/ontology> .
+}
+WHERE {
+  { ?s a owl:ObjectProperty }
+  UNION
+  { ?s a owl:Class }
+}
+`
+
 const store = new Store()
 for (const quad of dataset) {
   store.add(quad)
 }
-const inferred = doConstruct({ store, query })
+const inferredToExport = rdf.dataset()
+for (const query of [isDefinedBy, disjoints]) {
+  for (const quad of doConstruct({ store, query })) {
+    inferredToExport.add(quad)
+  }
+}
+
 const inferredFile = `${filePrefix}.owl.inferred.ttl`
 const prettyTurtle = await
-  prettyPrintTurtle({ dataset: inferred })
+  prettyPrintTurtle({ dataset: inferredToExport })
 writeFileSync(inferredFile, prettyTurtle)
-console.log('wrote', inferred.size, 'quads at', inferredFile)
+console.log('wrote', inferredToExport.size, 'quads at', inferredFile)
 
 /**
  * Write source errors
