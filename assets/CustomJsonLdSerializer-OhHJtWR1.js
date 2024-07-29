4b7d948710e2e4d39b715a718850e2244c04894a
cristianvasquez <cristianvasquez@users.noreply.github.com>
2024-07-29 14:05:05 +0000

    deploy: a93741853417f92f3e1c1253ea57b5bcf2bcafb3

--- assets/CustomJsonLdSerializer-OhHJtWR1.js
+++
@@ -1,1 +0,0 @@
-import{S as e}from"./index-v_KxaaEg.js";class t extends e{constructor({...r}={}){super({encoding:"string",...r})}}export{t as default};
--- assets/CustomRdfXmlParser-D0Emqbjr.js
+++
@@ -1,1 +0,0 @@
-import{r as s}from"./index-v_KxaaEg.js";class m extends s.RdfXmlParser{constructor({factory:r,...a}={}){super({...a,dataFactory:r})}}export{m as default};
