6ebc2f198128fe1a5b84f349792443432d6824db
cristianvasquez <cristianvasquez@users.noreply.github.com>
2024-11-27 09:18:03 +0000

    deploy: a1baa72e99824c2a273bc9c416e1756cab97f6cd

--- assets/CustomJsonLdSerializer-BVL_oy-x.js
+++
@@ -1,1 +0,0 @@
-import{S as e}from"./index-DMWX12a1.js";class t extends e{constructor({...r}={}){super({encoding:"string",...r})}}export{t as default};
--- assets/CustomRdfXmlParser-BMD9J3Kg.js
+++
@@ -1,1 +0,0 @@
-import{r as s}from"./index-DMWX12a1.js";class m extends s.RdfXmlParser{constructor({factory:r,...a}={}){super({...a,dataFactory:r})}}export{m as default};
