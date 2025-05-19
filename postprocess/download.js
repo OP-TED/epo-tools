d24f2e3a0c7008a1733ae393e0d5bce52b8fa1b5
Cristian <cristianvasquez@gmail.com>
2025-05-19 14:34:51 +0200

    - Application migrated to python

--- postprocess/download.js
+++
@@ -1,5 +0,0 @@
-import { fetchFromGithub } from '../src/download/github.js'
-
-import model from './model.json' with { type: 'json' }
-
-await fetchFromGithub(model)
--- postprocess/readme.md
+++
@@ -1,6 +0,0 @@
-## Contents
-
-
-- Temporal scripts until Model2Owl is fixed
-  - Generate SHACL from the model
-  - Remove unwanted elements from OWL
